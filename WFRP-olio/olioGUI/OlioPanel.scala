108fb62eece8e1dec8e91739612304e5bdc60a8d
Tuomas Anttila <tt_anttila@hotmail.com>
2017-05-15 12:16:43 +0300

    Careerin voi vaihtaa nyt tuplaklikkauksella.
    Tämä voisi olla hyvä tapa muokata ominaisuuksia: Label, jota voi
    tuplaklikata.

--- WFRP-olio/olioGUI/OlioPanel.scala
+++ WFRP-olio/olioGUI/OlioPanel.scala
@@ -8,18 +8,18 @@ import scala.swing.BorderPanel.Position._
 class OlioPanel(olio: Olio) extends BorderPanel {
   
   
-  val tPanel = this.topPanel(olio.name, olio.race, olio.career)
+  val tPanel = this.topPanel(olio.name, olio.race, olio.myCareer)
   
   this.layout(tPanel) = North
   
-  def topPanel(name: String, race: String, career: String): GridPanel = {
+  def topPanel(name: String, race: String, career: Career): GridPanel = {
     
     val tPanel = new GridPanel(1,3)
     val nameLabel = new Label(name)
+    val raceLabel = new Label(race)
+    val careerLabel = new Label(career.current)
     
     val rcPanel = new GridPanel(1,2) {
-      val raceLabel = new Label(race)
-      val careerLabel = new Label(career)
       contents += raceLabel
       contents += careerLabel
     }
@@ -60,6 +60,26 @@ class OlioPanel(olio: Olio) extends BorderPanel {
     tPanel.contents += leftPanel
     tPanel.contents += wfPanel
     tPanel.contents += nextDayButton
+    
+    
+    tPanel.listenTo(careerLabel.mouse.clicks)
+    
+    tPanel.reactions += {
+      case clickEvent: MouseClicked =>
+        if (clickEvent.clicks > 1) {
+          val input = Dialog.showInput(tPanel, "", initial = careerLabel.text)
+          input match {
+            case Some(n) => {
+              career.change(n)
+              careerLabel.text = n
+            }
+            case None =>
+          }
+        }
+        
+    }
+    
+    
     tPanel
   }
   
