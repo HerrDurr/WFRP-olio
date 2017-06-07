196995d80b3306e5afc613c53c4b2efc38261b72
Tuomas Anttila <tt_anttila@hotmail.com>
2017-06-07 12:52:57 +0300

    Aseet laitettu Tableen.
    Tablen saa jotenkin "wrapattua" ScrollPaneen, jolloin sitä voisi
    scrollata. Mitähän tämä käytännössä meinaa..

--- WFRP-olio/olioGUI/SkillPanel.scala
+++ WFRP-olio/olioGUI/SkillPanel.scala
@@ -5,14 +5,37 @@ import scala.swing._
 
 class SkillPanel(olio: Olio) extends GridPanel(3,1) {
   
+  val weapons = olio.weapons
   
-  val weaponPanel = new BoxPanel(Orientation.Vertical) {
-    olio.weapons.foreach(weapon => contents += new FlowPanel {
+  val weaponTable = new Table(3, 5) {
+    val scroller = new ScrollBar
+    scroller.orientation = Orientation.Vertical
+  }
+  
+  this.contents += weaponTable
+  
+  def update() = {
+    weapons.foreach {
+      weapon =>
+        val i = weapons.indexOf(weapon)
+        weaponTable.update(i, 0, weapon.name)
+        weaponTable.update(i, 1, weapon.group)
+        weaponTable.update(i, 2, weapon.damage(olio.attributes.strengthBonus).getOrElse("-"))
+        weaponTable.update(i, 3, weapon.range)
+        weaponTable.update(i, 4, weapon.qualities)
+    }
+  }
+  
+  /*
+  def update() = {
+    olio.weapons.foreach(weapon => weaponTable.contents += new FlowPanel {
       contents += new Label( weapon.name + ": " + weapon.group + ", " +
                             weapon.damage(olio.attributes.strengthBonus).getOrElse("-").toString() )
     })
   }
+  * 
+  */
   
-  this.contents += weaponPanel
+  update()
   
 }
