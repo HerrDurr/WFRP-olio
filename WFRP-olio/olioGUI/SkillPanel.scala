c8c5b3f80c020c5c52dcb0e1c01b1cc523e7049f
Unto <unto.solala@aalto.fi>
2017-06-13 20:52:40 +0300

    Kokeillaan taas committaa

--- WFRP-olio/olioGUI/SkillPanel.scala
+++ WFRP-olio/olioGUI/SkillPanel.scala
@@ -2,52 +2,35 @@ package olioGUI
 
 import main._
 import scala.swing._
-import data._
-import scala.io.Source
-import olioIO.WeaponIO
-//import javax.swing.table.DefaultTableModel
-//import collection.mutable.ArrayBuffer
+import collection.mutable.ArrayBuffer
+import javax.swing.table.DefaultTableModel
 
 class SkillPanel(olio: Olio) extends GridPanel(3,1) {
   
   val weapons = olio.weapons
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  val weaponData = ArrayBuffer(Array("Name", "Dmg", "Rng"), Array("Test", "-", "-"))
   //val weaponTable = new Table()
   
-  /*
-   * 
-   * val weaponData = Array(Array("Name", "Dmg", "Rng"), Array("Test", "-", "-"))
-  private var weaponTable = new Table(weaponData.tail.toArray.map(_.toArray[Any]), weaponData.head)
+  val weaponTable = new Table(weaponData.tail.toArray.map(_.toArray[Any]), weaponData.head) {
+    model = new DefaultTableModel
+  }
   
-  private var weaponScroller = new ScrollPane(weaponTable)
+  val tableModel  = weaponTable.model.asInstanceOf[DefaultTableModel]
+  
+  val weaponScroller = new ScrollPane(weaponTable)
   
   this.contents += weaponScroller
   
   def update() = {
-    val weaponArray: ArrayBuffer[Array[String]] = ArrayBuffer(Array("Name", "Dmg", "Rng"))
     weapons.foreach {
       weapon =>
-        //tableModel.addRow(Array[java.lang.Object] (weapon.name, weapon.damage(olio.attributes.strengthBonus).getOrElse('-').toString(), weapon.range))
+        tableModel.addRow(Array[java.lang.Object] (weapon.name, weapon.damage(olio.attributes.strengthBonus).getOrElse('-').toString(), weapon.range))
         //weaponData += Array(weapon.name, weapon.damage(olio.attributes.strengthBonus).getOrElse('-').toString(), weapon.range)
-        weaponArray += (Array(weapon.name, weapon.damage(olio.attributes.strengthBonus).getOrElse('-').toString(), weapon.range))
+        
         //weaponTable = new Table(weaponData.tail.toArray.map(_.toArray[Any]), weaponData.head)
         
     }
-    weaponTable = new Table(weaponArray.tail.toArray.map(_.toArray[Any]), weaponData.head)
-    weaponScroller = new ScrollPane(weaponTable)
-    repaint()
   }
-  * 
-  */
   
   /* uudempi
   def update() = {
@@ -74,5 +57,6 @@ class SkillPanel(olio: Olio) extends GridPanel(3,1) {
   * 
   */
   
+  update()
   
 }
--- WFRP-olio/olioIO/WeaponIO.scala
+++ WFRP-olio/olioIO/WeaponIO.scala
@@ -4,57 +4,8 @@ import java.io.BufferedReader
 import java.io.IOException
 import java.io.Reader
 import main.Weapon
-import scala.collection.mutable.Buffer
 
 object WeaponIO {
-  
-  def loadNames(input: Reader) = {
-    
-    val lineReader = new BufferedReader(input)
-    val names: Buffer[String] = Buffer()
-    
-    try {
-      
-      var currentLine = lineReader.readLine()
-      
-      findBlock()
-      
-      while (currentLine != null) {
-        names += currentLine.tail
-        currentLine = lineReader.readLine()
-        findBlock()
-      }
-      
-      def findBlock() = {
-        var done = false
-        while (!done && currentLine != null) {
-          currentLine = lineReader.readLine()
-          if (currentLine != null && !currentLine.isEmpty() && currentLine.head.toChar == '#') done = true
-        }
-      }
-    } catch {
-      
-      case e: IOException =>
-        val weaponsExc = new CorruptedWeaponsFileException("Reading the weapons data failed.")
-        weaponsExc.initCause(e)
-        throw weaponsExc
-      case e: NoSuchElementException =>
-        val weaponsExc = new CorruptedWeaponsFileException("A piece of data is missing.")
-        weaponsExc.initCause(e)
-        throw weaponsExc
-      case e: IndexOutOfBoundsException =>
-        val weaponsExc = new CorruptedWeaponsFileException("Reading the weapons data failed (index out of bounds).")
-        weaponsExc.initCause(e)
-        throw weaponsExc
-      case e: IllegalArgumentException =>
-        val weaponsExc = new CorruptedWeaponsFileException("Illegal argument.")
-        weaponsExc.initCause(e)
-        throw weaponsExc
-    }
-    
-    names.toVector
-    
-  }
   
   def loadWeapon(input: Reader, weapon: Weapon) = {
     
