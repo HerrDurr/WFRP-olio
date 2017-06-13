8cda382cf52d8ac4efdd84cdcd90ed697de4944e
Tuomas Anttila <tt_anttila@hotmail.com>
2017-06-13 20:25:00 +0300

    SkillPanel reworkattu: nyt kysyy WeaponIO:lta kaikkien aseiden nimet.
    Nimillä sitten tehdään valikko josta valita.

--- WFRP-olio/olioGUI/SkillPanel.scala
+++ WFRP-olio/olioGUI/SkillPanel.scala
@@ -2,35 +2,54 @@ package olioGUI
 
 import main._
 import scala.swing._
-import collection.mutable.ArrayBuffer
-import javax.swing.table.DefaultTableModel
+import data._
+import scala.io.Source
+import olioIO.WeaponIO
+//import javax.swing.table.DefaultTableModel
+//import collection.mutable.ArrayBuffer
 
 class SkillPanel(olio: Olio) extends GridPanel(3,1) {
   
   val weapons = olio.weapons
-  val weaponData = ArrayBuffer(Array("Name", "Dmg", "Rng"), Array("Test", "-", "-"))
+  
+  val reader = Source.fromFile("data/weapons.txt").reader()
+  
+  val weaponList = WeaponIO.loadNames(reader)
+  
+  
+  
+  
+  
+  
+  
+  
   //val weaponTable = new Table()
   
-  val weaponTable = new Table(weaponData.tail.toArray.map(_.toArray[Any]), weaponData.head) {
-    model = new DefaultTableModel
-  }
+  /*
+   * 
+   * val weaponData = Array(Array("Name", "Dmg", "Rng"), Array("Test", "-", "-"))
+  private var weaponTable = new Table(weaponData.tail.toArray.map(_.toArray[Any]), weaponData.head)
   
-  val tableModel  = weaponTable.model.asInstanceOf[DefaultTableModel]
-  
-  val weaponScroller = new ScrollPane(weaponTable)
+  private var weaponScroller = new ScrollPane(weaponTable)
   
   this.contents += weaponScroller
   
   def update() = {
+    val weaponArray: ArrayBuffer[Array[String]] = ArrayBuffer(Array("Name", "Dmg", "Rng"))
     weapons.foreach {
       weapon =>
-        tableModel.addRow(Array[java.lang.Object] (weapon.name, weapon.damage(olio.attributes.strengthBonus).getOrElse('-').toString(), weapon.range))
+        //tableModel.addRow(Array[java.lang.Object] (weapon.name, weapon.damage(olio.attributes.strengthBonus).getOrElse('-').toString(), weapon.range))
         //weaponData += Array(weapon.name, weapon.damage(olio.attributes.strengthBonus).getOrElse('-').toString(), weapon.range)
-        
+        weaponArray += (Array(weapon.name, weapon.damage(olio.attributes.strengthBonus).getOrElse('-').toString(), weapon.range))
         //weaponTable = new Table(weaponData.tail.toArray.map(_.toArray[Any]), weaponData.head)
         
     }
+    weaponTable = new Table(weaponArray.tail.toArray.map(_.toArray[Any]), weaponData.head)
+    weaponScroller = new ScrollPane(weaponTable)
+    repaint()
   }
+  * 
+  */
   
   /* uudempi
   def update() = {
@@ -57,6 +76,5 @@ class SkillPanel(olio: Olio) extends GridPanel(3,1) {
   * 
   */
   
-  update()
   
 }
