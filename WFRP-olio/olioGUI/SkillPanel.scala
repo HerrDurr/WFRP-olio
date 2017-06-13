3b5f8a188df1043ebb96c296daa0d03e4acceb74
Tuomas Anttila <tt_anttila@hotmail.com>
2017-06-13 18:53:53 +0300

    Weapon table laitettu scrollpaneen.
    update-metodi ei vielä toimi.

--- WFRP-olio/olioGUI/SkillPanel.scala
+++ WFRP-olio/olioGUI/SkillPanel.scala
@@ -2,18 +2,37 @@ package olioGUI
 
 import main._
 import scala.swing._
+import collection.mutable.ArrayBuffer
+import javax.swing.table.DefaultTableModel
 
 class SkillPanel(olio: Olio) extends GridPanel(3,1) {
   
   val weapons = olio.weapons
+  val weaponData = ArrayBuffer(Array("Name", "Dmg", "Rng"), Array("Test", "-", "-"))
+  //val weaponTable = new Table()
   
-  val weaponTable = new Table(3, 5) {
-    val scroller = new ScrollBar
-    scroller.orientation = Orientation.Vertical
+  val weaponTable = new Table(weaponData.tail.toArray.map(_.toArray[Any]), weaponData.head) {
+    model = new DefaultTableModel
   }
   
-  this.contents += weaponTable
+  val tableModel  = weaponTable.model.asInstanceOf[DefaultTableModel]
   
+  val weaponScroller = new ScrollPane(weaponTable)
+  
+  this.contents += weaponScroller
+  
+  def update() = {
+    weapons.foreach {
+      weapon =>
+        tableModel.addRow(Array[java.lang.Object] (weapon.name, weapon.damage(olio.attributes.strengthBonus).getOrElse('-').toString(), weapon.range))
+        //weaponData += Array(weapon.name, weapon.damage(olio.attributes.strengthBonus).getOrElse('-').toString(), weapon.range)
+        
+        //weaponTable = new Table(weaponData.tail.toArray.map(_.toArray[Any]), weaponData.head)
+        
+    }
+  }
+  
+  /* uudempi
   def update() = {
     weapons.foreach {
       weapon =>
@@ -25,8 +44,10 @@ class SkillPanel(olio: Olio) extends GridPanel(3,1) {
         weaponTable.update(i, 4, weapon.qualities)
     }
   }
+  * 
+  */
   
-  /*
+  /* vanhempi
   def update() = {
     olio.weapons.foreach(weapon => weaponTable.contents += new FlowPanel {
       contents += new Label( weapon.name + ": " + weapon.group + ", " +
