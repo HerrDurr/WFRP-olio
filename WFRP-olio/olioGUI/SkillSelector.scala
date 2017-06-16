21e9713b91b9ea8346f5c38af9d045647eea3a9f
Tuomas <Tuomas@DESKTOP-2QRALK0>
2017-06-16 21:08:44 +0300

    Kokeiltu samaa ComboBoxilla, mutta ongelma ei ollutkaan JSpinnerissä.
    (jeeee... -.-)

--- WFRP-olio/olioGUI/SkillSelector.scala
+++ WFRP-olio/olioGUI/SkillSelector.scala
@@ -3,22 +3,25 @@ package olioGUI
 import scala.swing._
 import main.Olio
 import main.Skill
-import javax.swing.SpinnerListModel
-import javax.swing.JSpinner
+//import javax.swing.SpinnerListModel
+//import javax.swing.JSpinner
 
 class SkillSelector (olio: Olio, skill: Skill) extends GridPanel(1, 3) {
   
   
   val nameLabel = new Label(skill.name + " (" + skill.skillAttribute._1 + ")")
   
-  val skillOptions: Array[Object] = Array("-", "X", "+10", "+20")
+  val skillOptions = Vector("-", "X", "+10", "+20")
   
-  val skillModel = new SpinnerListModel(skillOptions)
+  //val skillModel = new SpinnerListModel(skillOptions)
   
-  val spinner = Component.wrap(new JSpinner(skillModel))
+  //val spinner = Component.wrap(new JSpinner(skillModel))
+  
+  val selector = new ComboBox(skillOptions)
   
   val levelLabel = new Label(skill.skillLevel.toString())
   
-  this.contents += (nameLabel, spinner, levelLabel)
+  //this.contents += (nameLabel, spinner, levelLabel)
+  this.contents += (nameLabel, selector, levelLabel)
   
 }
