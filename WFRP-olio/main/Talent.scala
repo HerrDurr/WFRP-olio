5bdaad054b9d74225d8ea38d86d84877dedd59d6
Tuomas Anttila <tt_anttila@hotmail.com>
2017-06-17 22:47:13 +0300

    Talentille annettu vähän perusominaisuuksia

--- WFRP-olio/main/Talent.scala
+++ WFRP-olio/main/Talent.scala
@@ -2,4 +2,32 @@ package main
 
 class Talent(name: String) extends Loadable(name) {
   
+  private var attributeOption: Option[(String, Int)] = None
+  
+  private var skillOption: Option[Skill] = None
+  
+  private var descr = ""
+  
+  private var shortDescr = ""
+  
+  def affectedAttribute = this.attributeOption
+  
+  
+  def affectedSkill = this.skillOption
+  
+  
+  def description = this.descr
+  
+  
+  def shortDescription = this.shortDescr
+  
+  
+  def setAttribute(a: String, id: Int) = this.attributeOption = Option(a, id)
+  
+  
+  def setDescription(input: String) = this.descr = input
+  
+  
+  def setShortDescr(input: String) = this.shortDescr = input
+  
 }
