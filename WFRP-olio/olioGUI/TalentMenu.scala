68523ad11729499dc93410662bdac526d6fbdcba
Tuomas <Tuomas@DESKTOP-2QRALK0>
2017-06-21 17:20:22 +0300

    Talents.txt yks uus esimerkki, vähän muutettu

--- WFRP-olio/olioGUI/TalentMenu.scala
+++ WFRP-olio/olioGUI/TalentMenu.scala
@@ -5,6 +5,8 @@ import scala.swing._
 class TalentMenu(olioPanel: OlioPanel) extends BoxPanel(Orientation.Horizontal) {
   
   val lPanel = new BoxPanel(Orientation.Vertical)
+  val cPanel = new BoxPanel(Orientation.Vertical)
+  val rPanel = new BoxPanel(Orientation.Vertical)
   
   olioPanel.olio.allTalents.foreach(t => lPanel.contents += new Label(t.name))
   
