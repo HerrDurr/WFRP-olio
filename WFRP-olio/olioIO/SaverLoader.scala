f4e516982a7aa3d1106e0a135eeb9a0e086b7c77
Tuomas <Tuomas@DESKTOP-2QRALK0>
2017-06-21 20:45:51 +0300

    SaverLoader OlioIO tehty, seppo-esimerkkiä hahmoteltu

---
+++ WFRP-olio/olioIO/SaverLoader.scala
@@ -0,0 +1,29 @@
+package olioIO
+
+import java.io.IOException
+import java.io.PrintWriter
+import main.Olio
+import scala.io.Source
+
+object SaverLoader {
+  
+  
+  def loadOlio(file: Source) {
+    
+    
+    
+  }
+  
+  
+  
+  def saveOlio(olio: Olio) {
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+}
