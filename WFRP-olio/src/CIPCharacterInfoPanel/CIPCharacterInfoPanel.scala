f2c0fb3cf80c157f1107bc37857d81b2dfbdb341
Tuomas Anttila <tt_anttila@hotmail.com>
2017-12-25 23:13:53 +0200

    Prepared infopanel

--- WFRP-olio/src/CIPCharacterInfoPanel/CIPCharacterInfoPanel.scala
+++ WFRP-olio/src/CIPCharacterInfoPanel/CIPCharacterInfoPanel.scala
@@ -2,14 +2,23 @@ package CIPCharacterInfoPanel
 
 import scala.swing._
 import BorderPanel.Position._
+import main.Olio
 
-class CIPCharacterInfoPanel extends BorderPanel {
+class CIPCharacterInfoPanel(olio: Olio, fullView: Boolean) extends BorderPanel {
   
   val CIP1_1IconP = new CIP1_1IconPanel
   
   val CIP1_2ContainerP = new BorderPanel
   
+  val CIP1_2_1StatusAttackGrid = new GridPanel(1,2)
+  
+  val CIP1_2_2HealthGrid = new GridPanel(1,2)
+  
+  CIP1_2ContainerP.layout(CIP1_2_1StatusAttackGrid) = Center
+  CIP1_2ContainerP.layout(CIP1_2_2HealthGrid) = East
+  
   this.layout(CIP1_1IconP) = West
+  this.layout(CIP1_2ContainerP) = Center
   
   
   
